04396727d8339514966450a5b35af9562735519c
Cheong <xxlsxx1955@gmail.com>
2022-10-09 17:30:38 +0800

    Complete project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,68 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index(&block)
+    return enum_for(:my_each_with_index) unless block_given?
+
+    0..length.times do |index|
+      block.call(self[index], index)
+    end
+    self
+  end
+
+  def my_select(&block)
+    return enum_for(:my_select) unless block_given?
+
+    reduce([]) do |result, element|
+      result << element if block.call(element)
+      result
+    end
+  end
+
+  def my_all?(&block)
+    reduce(true) do |result, element|
+      result = false unless block.call(element)
+      result
+    end
+  end
+
+  def my_any?(&block)
+    reduce(false) do |result, element|
+      result = true if block.call(element)
+      result
+    end
+  end
+
+  def my_none?(&block)
+    reduce(true) do |result, element|
+      result = false if block.call(element)
+      result
+    end
+  end
+
+  def my_count(&block)
+    return length unless block_given?
+
+    reduce(0) do |counter, element|
+      counter += 1 if block.call(element)
+      counter
+    end
+  end
+
+  def my_map(&block)
+    return enum_for(:my_map) unless block_given?
+
+    reduce([]) do |result, element|
+      result << block.call(element)
+      result
+    end
+  end
+
+  def my_inject(init_value, &block)
+    for element in self
+      init_value = block.call(init_value, element)
+    end
+    init_value
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +71,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each(&block)
+    return enum_for(:my_each) unless block_given?
+
+    for element in self
+      block.call(element)
+    end
+  end
 end
